586743ac2f3ef0fc3ab3404ac99961afbac67524
partycrasher546 <muthonidennis546@gmail.com>
2024-06-09 00:18:58 +0300

    complete task

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,47 @@
 class Stack:
 
     def __init__(self, items = [], limit = 100):
-        pass
+        self.items = items
+        self.limit = limit
 
     def isEmpty(self):
-        pass
+        if self.items == []:
+            return True
+        else:
+            return False
 
     def push(self, item):
-        pass
+        qnty = len(self.items)
+        if qnty == self.limit:
+            return None
+        else:
+            self.items.append(item)
+            return self
 
     def pop(self):
-        pass
+        if self.isEmpty():
+            return None
+        else:
+            return self.items.pop()
 
     def peek(self):
         pass
     
     def size(self):
-        pass
+        return len(self.items)
 
     def full(self):
-        pass
+        if len(self.items) == self.limit:
+            return True
+        else:
+            return False
 
     def search(self, target):
-        pass
+        if target in self.items:
+            top = len(self.items)-1
+            index =self.items.index(target)
+            return top-index
+        else:
+            return -1
+
+            
